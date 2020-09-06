95ed846b10d4f120ec1e32a9c70095805e915e83
Martin <codingzhengsz@163.com>
2020-09-06 22:20:45 +0800

    [Martin] add Button logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button, {ButtonSize, ButtonType} from './components/Button/Button';
 
+/**
+ * It returns the App model
+ * @constructor
+ */
 function App() {
   return (
     <div className="App">
+      {/* eslint-disable-next-line max-len */}
+      <Button disabled>Hello</Button><Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Hello</Button>
+      <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled>Baidu Link</Button>
+      <button>123</button>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
